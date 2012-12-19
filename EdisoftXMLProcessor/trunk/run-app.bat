ec70a198d3359467e84462702931f0674019db86
informatik0101@gmail.com <informatik0101@gmail.com@55e0548d-92de-bb0c-35e3-05af340d623d>
2012-12-19 22:03:12 +0000

    Minor changes in ant build file and startup script.

--- EdisoftXMLProcessor/trunk/run-app.bat
+++ EdisoftXMLProcessor/trunk/run-app.bat
@@ -5,4 +5,4 @@ rem Target OS is MS Windows.
 rem For other OS write an appropriate script.
 
 rem Use of "chcp" command is for convenience: useful in case of outputting data encoded in UTF-8 to Windows console
-start cmd /k "chcp 65001>nul & java -Dfile.encoding=UTF8 -jar dist\EdisoftXMLProcessor.jar"
+start cmd /k "chcp 65001>nul & java -Dfile.encoding=UTF8 -jar EdisoftXMLProcessor.jar"
